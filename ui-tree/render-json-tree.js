aa0bd3e1dbd568108bf4d6d48bf8c567901f8a41
Anthony Bushinger <anthony.bushinger@metlife.com>
2022-01-16 17:38:57 -0500

    syncing deletes

--- ui-tree/render-json-tree.js
+++ ui-tree/render-json-tree.js
@@ -1,9 +1,13 @@
-function renderJsonTree(tree) {
-    const root = {
-        'root-model': tree,
-    };
 
-    function extractInfo(path) {
+class JsonTreeRender {
+    constructor() {
+        this.onNode = this.onNode.bind(this);
+        this.onLeaf = this.onLeaf.bind(this);
+        this.ids = {};
+        this.prevIds = {};
+    }
+
+    extractInfo(path) {
         const len = path.length;
         let name;
         const parentSegments = [];
@@ -23,35 +27,58 @@ function renderJsonTree(tree) {
         };
     }
 
-    function isValidPath(path) {
+    isValidPath(path) {
         return path.length > 1;
     }
 
-    const onNode = (node, path, _isArray) => {
-        if (!isValidPath(path)) {
+    updateIds(id) {
+        this.ids[id] = true;
+        if (this.prevIds[id]) {
+            delete this.prevIds[id];
+        }
+    }
+
+    onNode(node, path, _isArray) {
+        if (!this.isValidPath(path)) {
             return node;
         }
 
-        const { parentId, id, name } = extractInfo(path);
+        const { parentId, id, name } = this.extractInfo(path);
 
         const text = `${name} (${_isArray ? 'Array' : 'Object'})`;
 
         uiTreeApi.createNode(parentId, id, text);
 
+        this.updateIds(id);
+
         return node;
     };
 
-    const onLeaf = (value, path) => {
-        if (!isValidPath(path)) {
+    onLeaf(value, path) {
+        if (!this.isValidPath(path)) {
             return value;
         }
 
-        const { parentId, id, name } = extractInfo(path);
+        const { parentId, id, name } = this.extractInfo(path);
 
         uiTreeApi.createLeaf(parentId, id, name, value);
+
+        this.updateIds(id);
 
         return value;
     };
 
-    traverseObject(root, onNode, onLeaf);
+    render(tree) {
+        traverseObject(tree, this.onNode, this.onLeaf);
+
+        Object.keys(this.prevIds).forEach(uiTreeApi.removeNode);
+
+        this.prevIds = this.ids;
+        this.ids = {};
+    }
+}
+
+function createRenderer() {
+    const renderer = new JsonTreeRender();
+    return renderer.render.bind(renderer);
 }
